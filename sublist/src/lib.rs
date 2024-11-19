cc41f8e796a61a5fe4134d52b6badf99f01a44d5
Nam Nguyen <nam.nguyen.node@gmail.com>
2024-11-19 12:56:06 -0700

    sublist ✨

---
+++ sublist/src/lib.rs
@@ -0,0 +1,24 @@
+#[derive(Debug, PartialEq, Eq)]
+pub enum Comparison {
+    Equal,
+    Sublist,
+    Superlist,
+    Unequal,
+}
+
+pub fn sublist<T: PartialEq>(first_list: &[T], second_list: &[T]) -> Comparison {
+    let superlist = second_list.is_empty()
+        || first_list
+            .windows(second_list.len())
+            .any(|x| x == second_list);
+    let sublist = first_list.is_empty()
+        || second_list
+            .windows(first_list.len())
+            .any(|x| x == first_list);
+    match (superlist, sublist) {
+        (true, true) => Comparison::Equal,
+        (true, false) => Comparison::Superlist,
+        (false, true) => Comparison::Sublist,
+        (false, false) => Comparison::Unequal,
+    }
+}
