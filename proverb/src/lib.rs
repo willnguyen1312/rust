d997d072348978ad8b300e3b70882ce30b6d6db4
Nam Nguyen <nam.nguyen.node@gmail.com>
2024-11-20 11:10:19 -0700

    proverb ✨

---
+++ proverb/src/lib.rs
@@ -0,0 +1,12 @@
+use std::iter::once;
+
+pub fn build_proverb(list: &[&str]) -> String {
+    match list.first() {
+        None => String::new(),
+        Some(word) => list
+            .windows(2)
+            .map(|w| format!("For want of a {} the {} was lost.\n", w[0], w[1]))
+            .chain(once(format!("And all for the want of a {}.", word)))
+            .collect(),
+    }
+}
