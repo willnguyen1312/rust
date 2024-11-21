4b399ad33f15b4215fcad2b4fe1f24bb140b0660
Nam Nguyen <nam.nguyen.node@gmail.com>
2024-11-21 14:03:19 -0700

    poker ✨

---
+++ macros/src/lib.rs
@@ -0,0 +1,14 @@
+#[macro_export]
+macro_rules! hashmap {
+    () => {
+        ::std::collections::HashMap::new()
+    };
+
+    ( $($key:expr => $value:expr),+ $(,)? ) => {
+        {
+            let mut hm = ::std::collections::HashMap::new();
+            $( hm.insert($key, $value); )*
+            hm
+        }
+    };
+}
--- parallel-letter-frequency/src/lib.rs
+++ parallel-letter-frequency/src/lib.rs
@@ -1,11 +1,38 @@
 use std::collections::HashMap;
+use std::thread;
 
 pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
-    todo!(
-        "Count the frequency of letters in the given input '{input:?}'. Ensure that you are using {} to process the input.",
-        match worker_count {
-            1 => "1 worker".to_string(),
-            _ => format!("{worker_count} workers"),
+    let counter = |input: &[&str]| {
+        let mut map = HashMap::new();
+        for line in input {
+            for c in line
+                .chars()
+                .filter(|c| c.is_alphabetic())
+                .map(|c| c.to_ascii_lowercase())
+            {
+                *map.entry(c).or_default() += 1;
+            }
         }
-    );
+        map
+    };
+
+    match input.len() {
+        0 => HashMap::new(),
+        n if n < 500 => counter(input),
+        _ => thread::scope(|s| {
+            let mut handles = Vec::with_capacity(worker_count);
+            for lines in input.chunks(input.len() / worker_count + 1) {
+                handles.push(s.spawn(|| counter(lines)))
+            }
+
+            let mut map = handles.pop().unwrap().join().unwrap();
+            for res in handles {
+                res.join().unwrap().into_iter().for_each(|(k, v)| {
+                    *map.entry(k).or_default() += v;
+                })
+            }
+
+            map
+        }),
+    }
 }
