ff26cc15dc2a18a69e1812ef18b79b9993568f37
Nam Nguyen <nam.nguyen.node@gmail.com>
2024-11-21 11:47:15 -0700

    pangram ✨

---
+++ pangram/src/lib.rs
@@ -0,0 +1,12 @@
+use std::collections::HashSet;
+
+/// Determine whether a sentence is a pangram.
+pub fn is_pangram(sentence: &str) -> bool {
+    sentence
+        .to_lowercase()
+        .chars()
+        .filter(|c| c.is_ascii_alphabetic())
+        .collect::<HashSet<char>>()
+        .len()
+        == 26
+}
