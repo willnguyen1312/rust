cb801e5bbb33a0f7181f828d390b7f607d9eedcc
Nam Nguyen <nam.nguyen.node@gmail.com>
2024-11-21 13:35:02 -0700

    phone-number ✨

--- phone-number/src/lib.rs
+++ phone-number/src/lib.rs
@@ -1,5 +1,17 @@
 pub fn number(user_number: &str) -> Option<String> {
-    todo!(
-        "Given the number entered by user '{user_number}', convert it into SMS-friendly format. If the entered number is not a valid NANP number, return None."
-    );
+    let number: String = user_number.chars().filter(|c| c.is_numeric()).collect();
+
+    if number.len() == 10 && check_format(&number) {
+        Some(number)
+    } else if number.len() == 11 && &number[0..1] == "1" && check_format(&number[1..]) {
+        Some(String::from(&number[1..]))
+    } else {
+        None
+    }
+}
+
+fn check_format(number: &str) -> bool {
+    number
+        .char_indices()
+        .all(|(index, char)| (index == 0 || index == 3) && char >= '2' || index != 0 && index != 3)
 }
---
+++ rail-fence-cipher/src/lib.rs
@@ -0,0 +1,15 @@
+pub struct RailFence;
+
+impl RailFence {
+    pub fn new(rails: u32) -> RailFence {
+        todo!("Construct a new fence with {rails} rails")
+    }
+
+    pub fn encode(&self, text: &str) -> String {
+        todo!("Encode this text: {text}")
+    }
+
+    pub fn decode(&self, cipher: &str) -> String {
+        todo!("Decode this ciphertext: {cipher}")
+    }
+}
