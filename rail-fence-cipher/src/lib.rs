6ed7efda40e43dcc1dbd462555fbdf2efcccf0d6
Nam Nguyen <nam.nguyen.node@gmail.com>
2024-11-21 13:35:47 -0700

    rail-fence-cipher ✨

--- rail-fence-cipher/src/lib.rs
+++ rail-fence-cipher/src/lib.rs
@@ -1,15 +1,36 @@
-pub struct RailFence;
+pub struct RailFence {
+    rails: usize,
+}
 
 impl RailFence {
     pub fn new(rails: u32) -> RailFence {
-        todo!("Construct a new fence with {rails} rails")
+        RailFence {
+            rails: rails as usize,
+        }
     }
 
     pub fn encode(&self, text: &str) -> String {
-        todo!("Encode this text: {text}")
+        let mut result = vec![Vec::new(); self.rails];
+        for (c, i) in text.chars().zip(zigzag(self.rails)) {
+            result[i].push(c);
+        }
+        result.iter().flat_map(|c| c).collect::<String>()
     }
 
     pub fn decode(&self, cipher: &str) -> String {
-        todo!("Decode this ciphertext: {cipher}")
+        let mut indexes: Vec<_> = zigzag(self.rails).zip(1..).take(cipher.len()).collect();
+        indexes.sort();
+
+        let mut char_with_index: Vec<_> = cipher
+            .chars()
+            .zip(indexes)
+            .map(|(c, (_, i))| (i, c))
+            .collect();
+        char_with_index.sort();
+        char_with_index.iter().map(|(_, c)| c).collect()
     }
+}
+
+fn zigzag(n: usize) -> impl Iterator<Item = usize> {
+    (0..n - 1).chain((1..n).rev()).cycle()
 }
