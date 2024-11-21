77dd5233127acf30b9deb5654691d96b4371fd97
Nam Nguyen <nam.nguyen.node@gmail.com>
2024-11-21 12:14:42 -0700

    simple_linked_list ✨

---
+++ simple-linked-list/src/lib.rs
@@ -0,0 +1,85 @@
+use std::iter::FromIterator;
+
+pub struct SimpleLinkedList<T> {
+    head: Option<Box<Node<T>>>,
+}
+
+struct Node<T> {
+    data: T,
+    next: Option<Box<Node<T>>>,
+}
+
+impl<T> Node<T> {
+    fn new(data: T, next: Option<Box<Node<T>>>) -> Self {
+        Self { data, next }
+    }
+}
+
+impl<T> SimpleLinkedList<T> {
+    pub fn new() -> Self {
+        Self {
+            head: None
+        }
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.head.is_none()
+    }
+
+    pub fn len(&self) -> usize {
+        let mut current_node = &self.head;
+        let mut size = 0;
+        while let Some(x) = current_node {
+            size += 1;
+            current_node = &x.next;
+        }
+        size
+    }
+
+    pub fn push(&mut self, element: T) {
+        let node = Box::new(Node::new(element, self.head.take()));
+        self.head = Some(node);
+    }
+
+    pub fn pop(&mut self) -> Option<T> {
+        if self.head.is_some() {
+            let head_node = self.head.take().unwrap();
+            self.head = head_node.next;
+            Some(head_node.data)
+        } else {
+            None
+        }
+    }
+
+    pub fn peek(&self) -> Option<&T> {
+        self.head.as_ref().map(|head| &(head.data))
+    }
+
+    pub fn rev(mut self) -> SimpleLinkedList<T> {
+        let mut ret = SimpleLinkedList::new();
+        while let Some(x) = self.pop() {
+            ret.push(x);
+        }
+        ret
+    }
+}
+
+impl<T> FromIterator<T> for SimpleLinkedList<T> {
+    fn from_iter<I: IntoIterator<Item=T>>(iter: I) -> Self {
+        let mut list = SimpleLinkedList::new();
+        for item in iter {
+            list.push(item);
+        }
+        list
+    }
+}
+
+impl<T> Into<Vec<T>> for SimpleLinkedList<T> {
+    fn into(mut self) -> Vec<T> {
+        let mut ret = Vec::new();
+        while let Some(x) = self.pop() {
+            ret.insert(0, x);
+        }
+        ret
+    }
+}
